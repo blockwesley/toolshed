ebfb9c9ff04462365bf34997842342cf2e8e974b
Wesley Buttons <wesleybuttons@yahoo.com>
2025-02-23 12:30:43 +0000

    initial virtualization support checking script

---
+++ virtualization/inspect.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Bash Script to query some basic information about the host I am on.
+# Wesley 2025
+
+echo "The date today is: " `date`
+
+#Print Some Basic Info About the Host and Setup
+if [ -x "$(which hostname)" ]; then
+    echo "Hostname of this machine is: " `hostname`
+else
+    echo "Unable to find hostname command aborting execution of script."
+    exit 1
+fi
+
+#Grep the cpuinfo file and determine if 
+if [ -x "$(which grep)" ]; then
+    grep -E -wo 'svm|vmx|lm|aes' /proc/cpuinfo  | sort | uniq \
+    | sed -e 's/aes/Hardware encryption=Yes (&)/g' \
+    -e 's/lm/64 bit cpu=Yes (&)/g' -e 's/svm/AMD hardware virtualization=Yes (&)/g' \
+    -e 's/vmx/Intel hardware virtualization=Yes (&)/g'
+else
+    echo "Unable to determine virtualization support on this host."
+    exit 1
+fi
