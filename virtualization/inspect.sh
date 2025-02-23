ddbb0a488c1336a3dc0f49eaf4d8ca835c152bb5
Wesley Buttons <wesleybuttons@yahoo.com>
2025-02-23 20:04:39 +0000

    update

--- virtualization/inspect.sh
+++ virtualization/inspect.sh
@@ -12,6 +12,22 @@ else
     exit 1
 fi
 
+#Print Some Basic Info About the Host and Setup
+if [ -x "$(which free)" ]; then
+    # $total_memory = 
+    # total_memory = eval(`awk \'NR==2 {print $3}\'`)
+    # MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+    TOTAL_MEMORY=$(free -h | awk 'NR==2{print $2 }')
+    USED_MEMORY=$(free -h | awk 'NR==2{print $3 }')
+    FREE_MEMORY=$(free -h | awk 'NR==2{print $4 }')
+    # DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
+    # CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+    echo "Memory Information (Total/Used/Free): $TOTAL_MEMORY / $USED_MEMORY / $FREE_MEMORY"
+else
+    echo "Unable to find memory command aborting execution of script."
+    exit 1
+fi
+
 #Grep the cpuinfo file and determine if the host has hardware virtualization support
 # REF : https://www.cyberciti.biz/faq/linux-xen-vmware-kvm-intel-vt-amd-v-support/
 if [ -x "$(which grep)" ]; then
